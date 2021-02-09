e911a2e0cdd0bd109f4f420020c8993a75008f34
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-02-09 23:29:29 +0100

    Added FinishedRecipeAdapter(s) to handle NBT in datagen recipes output

---
+++ src/main/java/it/zerono/mods/zerocore/lib/datagen/provider/recipe/AbstractFinishedRecipeAdapter.java
@@ -0,0 +1,83 @@
+/*
+ *
+ * FinishedRecipeAdapter.java
+ *
+ * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.zerocore.lib.datagen.provider.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nullable;
+
+public abstract class AbstractFinishedRecipeAdapter
+        implements IFinishedRecipe {
+
+    //region IFinishedRecipe
+
+    @Override
+    public void serialize(final JsonObject json) {
+        this._originalRecipe.serialize(json);
+    }
+
+    /**
+     * Gets the ID for the recipe.
+     */
+    @Override
+    public ResourceLocation getID() {
+        return this._originalRecipe.getID();
+    }
+
+    @Override
+    public IRecipeSerializer<?> getSerializer() {
+        return this._serializer;
+    }
+
+    /**
+     * Gets the JSON for the advancement that unlocks this recipe. Null if there is no advancement.
+     */
+    @Nullable
+    @Override
+    public JsonObject getAdvancementJson() {
+        return this._originalRecipe.getAdvancementJson();
+    }
+
+    /**
+     * Gets the ID for the advancement associated with this recipe. Should not be null if {@link #getAdvancementJson} is
+     * non-null.
+     */
+    @Nullable
+    @Override
+    public ResourceLocation getAdvancementID() {
+        return this._originalRecipe.getAdvancementID();
+    }
+
+    //endregion
+    //region internals
+
+    protected AbstractFinishedRecipeAdapter(final IFinishedRecipe originalRecipe, final IRecipeSerializer<?> serializer) {
+
+        this._originalRecipe = originalRecipe;
+        this._serializer = serializer;
+    }
+
+    protected final IFinishedRecipe _originalRecipe;
+    protected final IRecipeSerializer<?> _serializer;
+
+    //endregion
+}
---
+++ src/main/java/it/zerono/mods/zerocore/lib/datagen/provider/recipe/NbtResultFinishedRecipeAdapter.java
@@ -0,0 +1,71 @@
+/*
+ *
+ * NbtResultFinishedRecipeAdapter.java
+ *
+ * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.zerocore.lib.datagen.provider.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.data.IFinishedRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.JSONUtils;
+
+import java.util.function.Consumer;
+
+public class NbtResultFinishedRecipeAdapter
+        extends AbstractFinishedRecipeAdapter {
+
+    public static Consumer<IFinishedRecipe> from(final Consumer<IFinishedRecipe> originalRecipe, final IRecipeSerializer<?> serializer,
+                                                 final CompoundNBT data)  {
+        return fr -> originalRecipe.accept(new NbtResultFinishedRecipeAdapter(fr, serializer, data));
+    }
+
+    public static Consumer<IFinishedRecipe> from(final Consumer<IFinishedRecipe> originalRecipe, final IRecipeSerializer<?> serializer,
+                                                 final Consumer<CompoundNBT> data) {
+
+        final CompoundNBT nbt = new CompoundNBT();
+
+        data.accept(nbt);
+        return from(originalRecipe, serializer, nbt);
+    }
+
+    //region IFinishedRecipe
+
+    @Override
+    public void serialize(final JsonObject json) {
+
+        super.serialize(json);
+
+        if (null != this._data) {
+            JSONUtils.getJsonObject(json, "result").addProperty("nbt", this._data.toString());
+        }
+    }
+
+    //endregion
+    //region internals
+
+    private NbtResultFinishedRecipeAdapter(final IFinishedRecipe originalRecipe, final IRecipeSerializer<?> serializer,
+                                           final CompoundNBT resultData) {
+
+        super(originalRecipe, serializer);
+        this._data = resultData;
+    }
+
+    private final CompoundNBT _data;
+
+    //endregion
+}
