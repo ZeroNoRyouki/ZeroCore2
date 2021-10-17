3ae29aa18f6a1ab6575ea97e8e9ae7971d562c7f
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-10-17 21:59:00 +0200

    Added base package - internal implementation of base classes

---
+++ src/main/java/it/zerono/mods/zerocore/base/CommonConstants.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * CommonConstants.java
+ *
+ * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.zerocore.base;
+
+public final class CommonConstants {
+
+    public static String COMMAND_ACTIVATE = "activate";
+    public static String COMMAND_DEACTIVATE = "deactivate";
+	public static String COMMAND_SET = "set";
+    public static String COMMAND_SET_INPUT = "setinput";
+    public static String COMMAND_SET_OUTPUT = "setoutput";
+    public static String COMMAND_SET_SENSOR = "setsensor";
+    public static String COMMAND_DISABLE_SENSOR = "nosensor";
+    public static String COMMAND_EJECT = "eject";
+
+    //region internals
+
+    private CommonConstants() {
+    }
+
+    //endregion
+}
