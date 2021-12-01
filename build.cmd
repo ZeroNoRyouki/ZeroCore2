97658655acf818300fbe039e1516faa6b1261350
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-12-01 19:47:49 +0100

    Fixed build cmds

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
 call gradlew.bat :ZeroCore2-1.17:build
-cd ZeroCore2-master
+cd ZeroCore2-1.17
--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd ..
 call gradlew.bat :ZeroCore2-1.17:publish
-cd ZeroCore2-master
+cd ZeroCore2-1.17
