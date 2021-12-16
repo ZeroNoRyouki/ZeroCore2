6912029963b3b998b4849c527703245ec6de4879
ZeroNoRyouki <zeronoryouki@gmail.com>
2021-12-16 20:50:55 +0100

    Fix dev-env detection

--- src/main/java/it/zerono/mods/zerocore/lib/CodeHelper.java
+++ src/main/java/it/zerono/mods/zerocore/lib/CodeHelper.java
@@ -51,6 +51,7 @@ import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fmllegacy.LogicalSidedProvider;
 
@@ -121,10 +122,7 @@ public final class CodeHelper {
     }
 
     public static boolean isDevEnv() {
-
-        final String target = System.getenv("target");
-
-        return !Strings.isNullOrEmpty(target) && target.startsWith("fmluserdev");
+        return !FMLEnvironment.production;
     }
 
     //region misc
