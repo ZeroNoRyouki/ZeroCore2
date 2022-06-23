6a4e353604acbf6d201d59c20a8cf7d1b4710ed6
ZeroNoRyouki <zeronoryouki@gmail.com>
2022-06-23 20:53:18 +0200

    Added back Patchouli support Updated Patchouli to version 1.19-72

--- src/main/java/it/zerono/mods/zerocore/lib/compat/patchouli/Patchouli.java
+++ src/main/java/it/zerono/mods/zerocore/lib/compat/patchouli/Patchouli.java
@@ -1,85 +1,85 @@
-///*
-// *
-// * Patchouli.java
-// *
-// * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
-// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// * DEALINGS IN THE SOFTWARE.
-// *
-// * DO NOT REMOVE OR EDIT THIS HEADER
-// *
-// */
-//
-//package it.zerono.mods.zerocore.lib.compat.patchouli;
-//
-//import com.google.common.collect.Maps;
-//import it.zerono.mods.zerocore.ZeroCore;
-//import it.zerono.mods.zerocore.internal.Log;
-//import it.zerono.mods.zerocore.lib.compat.Mods;
-//import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Crafting;
-//import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Multiblock;
-//import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Smelting;
-//import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Spotlight;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.level.block.state.BlockState;
-//import net.minecraftforge.client.model.data.EmptyModelData;
-//import net.minecraftforge.client.model.data.IModelData;
-//import vazkii.patchouli.api.IMultiblock;
-//import vazkii.patchouli.api.PatchouliAPI;
-//import vazkii.patchouli.client.book.template.BookTemplate;
-//
-//import java.util.Map;
-//import java.util.function.Function;
-//
-//public class Patchouli {
-//
-//    public static void registerMultiblock(final ResourceLocation id, final IMultiblock multiblock,
-//                                          final Function<BlockState, BlockState> renderBlockStateMappers,
-//                                          final Function<BlockState, IModelData> modelDataMapper) {
-//
-//        PatchouliAPI.get().registerMultiblock(id, multiblock);
-//        s_renderBlockStateMappers.put(multiblock, renderBlockStateMappers);
-//        s_modelDataMappers.put(multiblock, modelDataMapper);
-//    }
-//
-//    public static BlockState getRenderBlockStateFor(final IMultiblock multiblock, final BlockState blockState) {
-//        return s_renderBlockStateMappers.getOrDefault(multiblock, bs -> bs).apply(blockState);
-//    }
-//
-//    public static IModelData getModelDataFor(final IMultiblock multiblock, final BlockState blockState) {
-//        return s_modelDataMappers.getOrDefault(multiblock, b -> EmptyModelData.INSTANCE).apply(blockState);
-//    }
-//
-//    public static void initialize() {
-//
-//        Mods.PATCHOULI.ifPresent(() -> () -> {
-//
-//            if (s_init) {
-//                return;
-//            }
-//
-//            Log.LOGGER.info("Initializing Patchouli custom templates...");
-//
-//            BookTemplate.registerComponent(ZeroCore.newID("zcspt_multiblock"), Multiblock.class);
-//            BookTemplate.registerComponent(ZeroCore.newID("zcspt_spotlight"), Spotlight.class);
-//            BookTemplate.registerComponent(ZeroCore.newID("zcspt_crafting"), Crafting.class);
-//            BookTemplate.registerComponent(ZeroCore.newID("zcspt_smelting"), Smelting.class);
-//
-//            s_init = true;
-//        });
-//    }
-//
-//    //region internals
-//
-//    private static boolean s_init = false;
-//    private static final Map<IMultiblock, Function<BlockState, BlockState>> s_renderBlockStateMappers = Maps.newHashMap();
-//    private static final Map<IMultiblock, Function<BlockState, IModelData>> s_modelDataMappers = Maps.newHashMap();
-//
-//    //endregion
-//}
+/*
+ *
+ * Patchouli.java
+ *
+ * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.zerocore.lib.compat.patchouli;
+
+import com.google.common.collect.Maps;
+import it.zerono.mods.zerocore.ZeroCore;
+import it.zerono.mods.zerocore.internal.Log;
+import it.zerono.mods.zerocore.lib.compat.Mods;
+import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Crafting;
+import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Multiblock;
+import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Smelting;
+import it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage.Spotlight;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.client.model.data.EmptyModelData;
+import net.minecraftforge.client.model.data.IModelData;
+import vazkii.patchouli.api.IMultiblock;
+import vazkii.patchouli.api.PatchouliAPI;
+import vazkii.patchouli.client.book.template.BookTemplate;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class Patchouli {
+
+    public static void registerMultiblock(final ResourceLocation id, final IMultiblock multiblock,
+                                          final Function<BlockState, BlockState> renderBlockStateMappers,
+                                          final Function<BlockState, IModelData> modelDataMapper) {
+
+        PatchouliAPI.get().registerMultiblock(id, multiblock);
+        s_renderBlockStateMappers.put(multiblock, renderBlockStateMappers);
+        s_modelDataMappers.put(multiblock, modelDataMapper);
+    }
+
+    public static BlockState getRenderBlockStateFor(final IMultiblock multiblock, final BlockState blockState) {
+        return s_renderBlockStateMappers.getOrDefault(multiblock, bs -> bs).apply(blockState);
+    }
+
+    public static IModelData getModelDataFor(final IMultiblock multiblock, final BlockState blockState) {
+        return s_modelDataMappers.getOrDefault(multiblock, b -> EmptyModelData.INSTANCE).apply(blockState);
+    }
+
+    public static void initialize() {
+
+        Mods.PATCHOULI.ifPresent(() -> () -> {
+
+            if (s_init) {
+                return;
+            }
+
+            Log.LOGGER.info("Initializing Patchouli custom templates...");
+
+            BookTemplate.registerComponent(ZeroCore.newID("zcspt_multiblock"), Multiblock.class);
+            BookTemplate.registerComponent(ZeroCore.newID("zcspt_spotlight"), Spotlight.class);
+            BookTemplate.registerComponent(ZeroCore.newID("zcspt_crafting"), Crafting.class);
+            BookTemplate.registerComponent(ZeroCore.newID("zcspt_smelting"), Smelting.class);
+
+            s_init = true;
+        });
+    }
+
+    //region internals
+
+    private static boolean s_init = false;
+    private static final Map<IMultiblock, Function<BlockState, BlockState>> s_renderBlockStateMappers = Maps.newHashMap();
+    private static final Map<IMultiblock, Function<BlockState, IModelData>> s_modelDataMappers = Maps.newHashMap();
+
+    //endregion
+}
--- src/main/java/it/zerono/mods/zerocore/lib/compat/patchouli/component/standardpage/AbstractRecipePage.java
+++ src/main/java/it/zerono/mods/zerocore/lib/compat/patchouli/component/standardpage/AbstractRecipePage.java
@@ -1,75 +1,75 @@
-///*
-// *
-// * AbstractRecipePage.java
-// *
-// * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
-// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// * DEALINGS IN THE SOFTWARE.
-// *
-// * DO NOT REMOVE OR EDIT THIS HEADER
-// *
-// */
-//
-//package it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage;
-//
-//import com.google.gson.annotations.SerializedName;
-//import it.zerono.mods.zerocore.internal.Log;
-//import net.minecraft.resources.ResourceLocation;
-//import vazkii.patchouli.api.IVariable;
-//import vazkii.patchouli.client.book.page.abstr.PageDoubleRecipe;
-//
-//import java.lang.reflect.Field;
-//import java.util.function.UnaryOperator;
-//
-//public abstract class AbstractRecipePage<T, PageType extends PageDoubleRecipe<T>>
-//    extends AbstractTextPage<PageType> {
-//
-//    @SerializedName("recipe") String recipeId;
-//    @SerializedName("recipe2") String recipe2Id;
-//    String title;
-//
-//    protected AbstractRecipePage(final PageType page) {
-//        super(page);
-//    }
-//
-//    //region AbstractStandardPageComponent
-//
-//    @Override
-//    public void onVariablesAvailable(UnaryOperator<IVariable> lookup) {
-//
-//        super.onVariablesAvailable(lookup);
-//
-//        try {
-//
-//            recipe1Field.set(this.getPage(), new ResourceLocation(lookup.apply(IVariable.wrap(recipeId)).asString()));
-//            recipe2Field.set(this.getPage(), new ResourceLocation(lookup.apply(IVariable.wrap(recipe2Id)).asString()));
-//            titleField.set(this.getPage(), lookup.apply(IVariable.wrap(title)).asString());
-//
-//        } catch (IllegalAccessException e) {
-//
-//            Log.LOGGER.warn(Log.CORE, "patchouli AbstractRecipePage wrapper : Unable to set inner page fields");
-//        }
-//    }
-//
-//    //endregion
-//    //region internals
-//
-//    private static final Field recipe1Field;
-//    private static final Field recipe2Field;
-//    private static final Field titleField;
-//
-//    static {
-//
-//        recipe1Field = getField(PageDoubleRecipe.class, "recipeId");
-//        recipe2Field = getField(PageDoubleRecipe.class, "recipe2Id");
-//        titleField = getField(PageDoubleRecipe.class, "title");
-//    }
-//
-//    //endregion
-//}
+/*
+ *
+ * AbstractRecipePage.java
+ *
+ * This file is part of Zero CORE 2 by ZeroNoRyouki, a Minecraft mod.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * DO NOT REMOVE OR EDIT THIS HEADER
+ *
+ */
+
+package it.zerono.mods.zerocore.lib.compat.patchouli.component.standardpage;
+
+import com.google.gson.annotations.SerializedName;
+import it.zerono.mods.zerocore.internal.Log;
+import net.minecraft.resources.ResourceLocation;
+import vazkii.patchouli.api.IVariable;
+import vazkii.patchouli.client.book.page.abstr.PageDoubleRecipe;
+
+import java.lang.reflect.Field;
+import java.util.function.UnaryOperator;
+
+public abstract class AbstractRecipePage<T, PageType extends PageDoubleRecipe<T>>
+    extends AbstractTextPage<PageType> {
+
+    @SerializedName("recipe") String recipeId;
+    @SerializedName("recipe2") String recipe2Id;
+    String title;
+
+    protected AbstractRecipePage(final PageType page) {
+        super(page);
+    }
+
+    //region AbstractStandardPageComponent
+
+    @Override
+    public void onVariablesAvailable(UnaryOperator<IVariable> lookup) {
+
+        super.onVariablesAvailable(lookup);
+
+        try {
+
+            recipe1Field.set(this.getPage(), new ResourceLocation(lookup.apply(IVariable.wrap(recipeId)).asString()));
+            recipe2Field.set(this.getPage(), new ResourceLocation(lookup.apply(IVariable.wrap(recipe2Id)).asString()));
+            titleField.set(this.getPage(), lookup.apply(IVariable.wrap(title)).asString());
+
+        } catch (IllegalAccessException e) {
+
+            Log.LOGGER.warn(Log.CORE, "patchouli AbstractRecipePage wrapper : Unable to set inner page fields");
+        }
+    }
+
+    //endregion
+    //region internals
+
+    private static final Field recipe1Field;
+    private static final Field recipe2Field;
+    private static final Field titleField;
+
+    static {
+
+        recipe1Field = getField(PageDoubleRecipe.class, "recipeId");
+        recipe2Field = getField(PageDoubleRecipe.class, "recipe2Id");
+        titleField = getField(PageDoubleRecipe.class, "title");
+    }
+
+    //endregion
+}
