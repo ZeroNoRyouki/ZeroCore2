a4047e4fa1eb768591b8d82f3c5097d4a8367ed9
ZeroNoRyouki <zeronoryouki@gmail.com>
2024-06-20 20:36:44 +0200

    RecipeInput

--- src/main/java/it/zerono/mods/zerocore/lib/item/inventory/EmptyVanillaInventory.java
+++ src/main/java/it/zerono/mods/zerocore/lib/item/inventory/EmptyVanillaInventory.java
@@ -18,76 +18,29 @@
 
 package it.zerono.mods.zerocore.lib.item.inventory;
 
-import net.minecraft.world.Container;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeInput;
 
-public class EmptyVanillaInventory
-    implements Container {
+public final class EmptyVanillaInventory
+    implements RecipeInput {
 
     public static final EmptyVanillaInventory INSTANCE = new EmptyVanillaInventory();
 
-    //region IInventory
+    //region RecipeInput
 
-    /**
-     * Returns the number of slots in the inventory.
-     */
     @Override
-    public int getContainerSize() {
+    public ItemStack getItem(int index) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public int size() {
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
         return true;
-    }
-
-    /**
-     * Returns the stack in the given slot.
-     */
-    @Override
-    public ItemStack getItem(int index) {
-        return ItemStack.EMPTY;
-    }
-
-    /**
-     * Removes up to a specified number of items from an inventory slot and returns them in a new stack.
-     */
-    @Override
-    public ItemStack removeItem(int index, int count) {
-        return ItemStack.EMPTY;
-    }
-
-    /**
-     * Removes a stack from the given slot and returns it.
-     */
-    @Override
-    public ItemStack removeItemNoUpdate(int index) {
-        return ItemStack.EMPTY;
-    }
-
-    /**
-     * Sets the given item stack to the specified slot in the inventory (can be crafting or armor sections).
-     */
-    @Override
-    public void setItem(int index, ItemStack stack) {
-    }
-
-    /**
-     * For tile entities, ensures the chunk containing the tile entity is saved to disk later - the game won't think it
-     * hasn't changed and skip it.
-     */
-    @Override
-    public void setChanged() {
-    }
-
-    @Override
-    public boolean stillValid(Player player) {
-        return false;
-    }
-
-    @Override
-    public void clearContent() {
     }
 
     //endregion
