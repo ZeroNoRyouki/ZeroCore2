473b2ba4af10ec6153a7d6bef471031b11ba2ea9
ZeroNoRyouki <zeronoryouki@gmail.com>
2024-08-13 15:55:27 +0200

    Forge only init capabilities for BucketItem so let's init them ourself. Close ER #160

--- src/main/java/it/zerono/mods/zerocore/lib/item/TintedBucketItem.java
+++ src/main/java/it/zerono/mods/zerocore/lib/item/TintedBucketItem.java
@@ -1,11 +1,15 @@
 package it.zerono.mods.zerocore.lib.item;
 
 import com.google.common.base.Preconditions;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.BiFunction;
 
@@ -35,6 +39,14 @@ public class TintedBucketItem
         return 0xFFFFFFFF;
     }
 
+    //region BucketItem
+
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundTag nbt) {
+        return new FluidBucketWrapper(stack);
+    }
+
+    //endregion
     //region internals
 
     private final BiFunction<@NotNull ItemStack, Integer, Integer> _colorProvider;
